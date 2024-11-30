3f726e5eefc46b4d90ebda18484851d2557c64df
Mustafa Osmanoglu <143639628+Mustafa-Osmanoglu@users.noreply.github.com>
2024-11-30 19:09:34 +0300

    Create veri-tipleri.py

---
+++ veri-tipleri.py
@@ -0,0 +1,11 @@
+isim = "Çınar"
+yas = 7
+kilo = 15.5
+ogrenciMi = True
+
+print(type(isim))
+print(type(yas))
+print(type(kilo))
+print(type(ogrenciMi))
+
+print(" ad: " + isim + " yaş: " + str(yas) + " kilo: " + str(kilo) + " öğrenci mi: " + str(ogrenciMi))
